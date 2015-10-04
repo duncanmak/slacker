0822f9bcf76121bbf9249275a20aafc0d6ed0d78
Duncan Mak <duncan.mak@xamarin.com>
2015-10-04 00:13:17 -0400

    First attempt

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-const Slack = require('slack-client');
-const token = '';
+// Based on http://blog.somewhatabstract.com/2015/03/02/writing-a-simple-slack-bot-with-node-slack-client/
 
-let slack = new Slack(token, true, true);
+import Client = require('slack-client');
 
-slack.on('open', () => {
-    let channels = Object.keys(slack.channels)
+const token = process.env['SLACK_TOKEN'];
+const slack = new Client(token);
+
+slack.on('open', function() {
+    var channels = Object.keys(slack.channels)
         .map(k => slack.channels[k])
         .filter(c => c.is_member)
         .map(c => c.name);
@@ -14,16 +16,31 @@ slack.on('open', () => {
         .filter(g => g.is_open && !g.is_archived)
         .map(g => g.name);
 
-    console.log(`Welcome to Slack. You are ${slack.self.name} of ${slack.team.name}.`);
+    console.log(`Welcome to Slack. You are ${slack.self.name} of ${slack.team.name}`);
 
     if (channels.length > 0) {
-        console.log('You are in: ' + channels.join(', '));
+        console.log(`You are in: ${channels.join(', ') }`);
     }
     else {
         console.log('You are not in any channels.');
     }
 
     if (groups.length > 0) {
-       console.log('As well as: ' + groups.join(', '));
+        console.log(`As well as: ${groups.join(', ') }`);
     }
 });
+
+slack.on('message', (message) => {
+
+    console.log(`I heard ${JSON.stringify(message)}`);
+
+    let channel = slack.getChannelGroupOrDMByID(message.channel);
+    let user = slack.getUserByID(message.user);
+
+    if (message.text.includes('bye')) {
+        channel.send('Disconnecting');
+        slack.disconnect();
+    }
+});
+
+slack.login();
--- src/slack-client.d.ts
+++ src/slack-client.d.ts
@@ -1,2 +1,116 @@
-declare module "Slack" {
+declare module 'slack-client' {
+    import { EventEmitter } from 'events';
+
+    class Client extends EventEmitter {
+        constructor(token: string, autoReconnect?: boolean, autoMark?: boolean);
+
+        self: User;
+        team: Team;
+
+        channels: Channel[];
+        dms: any[];
+        groups: Group[];
+        users: User[];
+        bots: Bot[];
+
+        socketUrl: string;
+
+        login();
+        connect();
+        disconnect();
+        reconnect();
+
+        joinChannel(name: string, callback: Function);
+        openDM(user_id: string, callback: Function);
+
+        createGroup (name: string, callback: Function);
+
+        setPresence(presence, callback);
+        setActive(callback);
+
+        getUserByID(id): User;
+        getUserByName(name: string): User;
+        getChannelByID(id): Channel;
+        getChannelByName(name: string): Channel;
+        getDMByID(id);
+        getDMByName(name: string);
+        getGroupByID(id): Group;
+        getGroupByName(name: string): Group;
+        getChannelGroupOrDMByID(id): Channel|Group|DM;
+        getChannelGroupOrDMByName(name: string)
+
+        getUnreadCount(): number;
+
+        getChannelsWithUnreads(): Channel[];
+    }
+
+    interface Channel {
+
+        id: number;
+        topic: string;
+        purpose: string;
+        name: string;
+        members: User[];
+        unread_count: number;
+        latest: Message;
+
+        addMessage(message);
+        getHistory();
+
+        startedTyping(user_id: number);
+        getTyping();
+
+        send(text: string);
+        postMessage(data);
+        sendMessage(message);
+        fetchHistory(latest, oldest);
+
+        mark(ts);
+        leave();
+
+        setTopic(topic: string);
+        setPurpose(purpose: string);
+        rename(name: string);
+        invite(user_id: number);
+    }
+
+    interface Group extends Channel {
+        close();
+        open();
+        archive();
+        unarchive();
+    }
+
+    interface DM extends Channel {
+        close();
+    }
+
+    interface Team {
+        name: string;
+    }
+    interface User {
+        name: string;
+    }
+    interface Bot {
+        name: string;
+    }
+
+    interface Message {
+
+        id: number;
+        type: string;
+        channel: Channel;
+        text: string;
+        user: User;
+        username: string;
+
+        toJSON();
+        getBody(): string;
+        toString(): string;
+        getChannelType();
+        updateMessage(new_text: string);
+        deleteMessage();
+    }
+
+    export = Client;
 }
